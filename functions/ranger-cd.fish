3b0dd384f8747b18c3c2b32e49a9c0e9102063d9
Gabriel Saillard <gabriel@saillard.dev>
2022-09-04 16:21:33 +0200

    fix: hardcoded ranger path allow aliasing
    by hardcoding and using a fixed path to ranger, i can alias `ranger` to `cd-ranger` directly, making cd-on-exit default behavior

--- functions/ranger-cd.fish
+++ functions/ranger-cd.fish
@@ -1,10 +1,12 @@
 # ranger-cd.fish
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+set -ug RANGER_PATH '/opt/homebrew/bin/ranger'
+
 function ranger-cd -w 'ranger' -d 'Automatically change the directory in fish after closing ranger'
     set -l tempfile (mktemp -t tmp.XXXXXX)
 
-    ranger --choosedir=$tempfile $argv
+    $RANGER_PATH --choosedir=$tempfile $argv
 
     if [ -f $tempfile ]
         cd (cat $tempfile)
